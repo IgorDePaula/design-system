51ff2a5633a99296cc5769aaad98256acaa7c579
Igor de Paula <principe.borodin@gmail.com>
2022-11-08 13:17:17 -0300

    fix: fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import {Avatar,Badget,Button,Table,Label,Link,Loading,Option,TextInput} from './components'
 import {formatPrice,classNames} from './functions'
-/*export * from './types'*/
+
 export  {Avatar,Badget,Button,Table,Label,Link,Loading,Option,TextInput,formatPrice,classNames}
