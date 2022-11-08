028f4fa15c3e49487fc871d9362670c1a98bda02
Igor de Paula <principe.borodin@gmail.com>
2022-11-08 12:44:04 -0300

    refactor: fixed export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-export * from './components';
-export * from './functions';
-export * from './types';
+import { Avatar, Badget, Button, Table, Label, Link, Loading, Option, TextInput } from './components';
+import { formatPrice, classNames } from './functions';
+export { Avatar, Badget, Button, Table, Label, Link, Loading, Option, TextInput, formatPrice, classNames };
 //# sourceMappingURL=index.d.ts.map
