d4f4e480830d92db9a1f20564de661dc69c20b52
Igor de Paula <principe.borodin@gmail.com>
2022-11-14 07:14:23 -0300

    fix: input export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,13 @@
-import { Avatar, Badget, Button, Table, Label, Link, Loading, Option } from './components';
+import { Avatar, Badget, Button, Table, Label, Link, Loading, Option, Input } from './components';
 import { formatPrice, classNames } from './functions';
 import type { AvatarType } from '@components/avatar';
 import type { BadgetType } from "@components/badget";
 import type { ButtonType } from "@components/button";
 import type { LabelType } from "@components/label";
 import type { ThemeType } from "@components/loading";
+import type { InputType } from "@components/input";
 import type { MenuType, MenuItemType } from "@components/option";
 import type { TableType, DataTableType } from "@components/table";
-export type { AvatarType, BadgetType, ButtonType, LabelType, MenuType, MenuItemType, ThemeType, TableType, DataTableType, };
-export { Avatar, Badget, Button, Table, Label, Link, Loading, Option, formatPrice, classNames };
+export type { AvatarType, BadgetType, ButtonType, LabelType, MenuType, MenuItemType, ThemeType, TableType, DataTableType, InputType };
+export { Avatar, Badget, Button, Table, Label, Link, Loading, Option, Input, formatPrice, classNames };
 //# sourceMappingURL=index.d.ts.map
