24b57da6d19dbbbad5dc0d46c59d2547e85c55d9
Igor de Paula <principe.borodin@gmail.com>
2022-11-14 07:12:07 -0300

    fix: input export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import {Avatar, Badget, Button, Table, Label, Link, Loading, Option} from './components'
+import {Avatar, Badget, Button, Table, Label, Link, Loading, Option, Input} from './components'
 import {formatPrice, classNames} from './functions'
 import type {AvatarType} from '@components/avatar'
 import type {BadgetType} from "@components/badget";
 import type {ButtonType} from "@components/button";
 import type {LabelType} from "@components/label";
 import type {ThemeType} from "@components/loading"
+import type {InputType} from "@components/input"
 import type {MenuType, MenuItemType} from "@components/option"
 import type {TableType, DataTableType} from "@components/table"
 
@@ -18,6 +19,7 @@ export type {
     ThemeType,
     TableType,
     DataTableType,
+    InputType
 }
 
 export {
@@ -29,6 +31,7 @@ export {
     Link,
     Loading,
     Option,
+    Input,
     formatPrice,
     classNames
 }
